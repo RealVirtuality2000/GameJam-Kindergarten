39cb80ca4a9e51ca187bc6c13ae4f434275c0449
Sarah Gaul <73908353+Wildfreund@users.noreply.github.com>
2022-02-26 10:19:37 +0100

    Doorlogic mit Steinen verknüpft

--- TEST/Assets/GameAssets/Skripte/DoorDelete.cs
+++ TEST/Assets/GameAssets/Skripte/DoorDelete.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class DoorDelete : MonoBehaviour
 {
-   
+    [SerializeField] private int StoneIndex;
+
+    private void Start()
+    {
+        if(DoorVariables.DoorsOpen[StoneIndex] == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
