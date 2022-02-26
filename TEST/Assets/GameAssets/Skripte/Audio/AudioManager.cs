b8c049a05470af597e03c5db4bea75296a9d56ae
Sarah Gaul <73908353+Wildfreund@users.noreply.github.com>
2022-02-26 23:47:31 +0100

    Volume verknüpft

--- TEST/Assets/GameAssets/Skripte/Audio/AudioManager.cs
+++ TEST/Assets/GameAssets/Skripte/Audio/AudioManager.cs
@@ -6,6 +6,7 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
     public static AudioManager instance;
+    public AudioMixerGroup audioMixer;
 
     private void Awake()
     {
@@ -22,12 +23,14 @@ public class AudioManager : MonoBehaviour
         DontDestroyOnLoad(this);
         foreach(Sound s in sounds)
         {
+
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = audioMixer;
         }
     }
 
